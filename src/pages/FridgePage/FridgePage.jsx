ada416af1c232d5bedf7e55f3ce43b3d304cb4a1
pwong09 <pwong09@gmail.com>
2022-05-23 16:51:28 -0700

    add embed google map functionality

--- src/pages/FridgePage/FridgePage.jsx
+++ src/pages/FridgePage/FridgePage.jsx
@@ -13,8 +13,16 @@ export default function FridgePage({user}) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
 
-    const key = process.env.API_KEY
+    const key = process.env.REACT_APP_GOOGLE_EMBED_API;
+    // let map;
 
+    // function initMap() {
+    //     map = new google.maps.Map(document.getElementById("map"), {
+    //     center: { lat: -34.397, lng: 150.644 },
+    //     zoom: 8,
+    //     });
+    // }
+    
     const getFridges = async () => {
         try {
             const data = await fridgesAPI.getAll();
@@ -28,6 +36,7 @@ export default function FridgePage({user}) {
 
     useEffect(() => {
         getFridges();
+        initMap();
     }, [])
 
     const handleAddFridge = async (fridge) => {
@@ -76,7 +85,12 @@ export default function FridgePage({user}) {
                 : null}
             </Grid.Row>
             <Grid.Row>
-            <iframe title="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d20820.169259534316!2d-123.14912855!3d49.285470000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673c6c965e05d%3A0xd7cf0c9485a4a040!2sKinara%20Indian%20Cuisine!5e0!3m2!1sen!2sca!4v1653344116913!5m2!1sen!2sca" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <iframe title="googleMap" 
+            src={`https://www.google.com/maps/embed/v1/search?key=${key}&q=community+fridge+in+Vancouver`}
+            width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+            </Grid.Row>
+            <Grid.Row>
+                <div id='map'></div>
             </Grid.Row>
             <Grid.Row>
                 <Grid.Column>
