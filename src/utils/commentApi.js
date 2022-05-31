9a4f9b283a9c6f65881ee4c69c4faa0f85185e24
pwong09 <pwong09@gmail.com>
2022-05-30 19:42:50 -0700

    POST comments 201

--- src/utils/commentApi.js
+++ src/utils/commentApi.js
@@ -1,13 +1,13 @@
 import tokenService from "./tokenService";
 const BASE_URL = '/api/'
 
-export function create(postId, comment) {
-    return fetch(`${BASE_URL}fridges/${postId}/comments`, {
+export function create(fridgeId, comment) {
+    console.log(comment)
+    return fetch(`${BASE_URL}fridges/${fridgeId}/comments`, {
         method: 'POST',
-        body: JSON.stringify(comment),
+        body: comment,
         headers: {
             'Authorization': 'Bearer ' + tokenService.getToken(),
-            "Content-Type": "application/json"
         }
     }).then((res) => {
         if (res.ok) return res.json();
